2214153c15a0f8973f31c70496982eb94da8dc1d
kylebyrne <kyletbyrne96@gmail.com>
2018-12-20 18:57:53 +0000

    fuck about

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 require('normalize.css/normalize.css');
 require('./application.scss');
 
-let circle = [..document.querySelector("#test"),
-  circleCenter = [circle.offsetLeft + 16, circle.offsetTop + 16]
+import { throttle } from 'lodash';
+
+let circles = [...document.querySelectorAll(".circle")]
 
 const mouseMove = (e) => {
-  let angle = Math.atan2(e.pageX - circleCenter[0], -(e.pageY - circleCenter[1])) * (180/Math.PI)
+  let mouseX = e.pageX, mouseY = e.pageY, radToDeg = 180 / Math.PI
+  circles.forEach((circle) => {
+    let circleCenter = [circle.offsetLeft + 16, circle.offsetTop + 16],
+      angle = Math.atan2(mouseX - circleCenter[0], -(mouseY - circleCenter[1])) * radToDeg
 
-  circle.style.transform = `rotate(${angle}deg)`
+    circle.style.transform = `rotate(${angle}deg)`
+  })
 }
 
-document.addEventListener("mousemove", mouseMove)
+document.addEventListener("mousemove", throttle(mouseMove, 200))
 
 
 
